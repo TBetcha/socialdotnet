bf556ffafca896ac8910232e80fb1360e6b027f5
TMB <Troy.Boettger@gmail.com>
2020-12-14 02:33:12 -0500

    add react frontend

---
+++ client-app/src/App.tsx
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import { Header, Icon, List } from 'semantic-ui-react'
+
+class App extends Component {
+  state = {
+    values: []
+  }
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/values')
+    .then((response) => {
+      this.setState({
+        values: response.data
+      })
+     })
+  
+  }
+  render(){
+    return (
+      <div className="App">
+        <Header as='h2'>
+          <Icon name='plug' />
+          <Header.Content>Uptime Guarantee</Header.Content>
+        </Header>
+        <List>
+          <List.Item>  {this.state.values.map((value: any) => (
+              <li key={value.id}>{value.name}</li>
+            ))}</List.Item>
+        </List>
+      </div>
+    );
+  }
+}
+
+export default App;
