0a291ff090462d112c14badbf7820b156de25849
TMB <Troy.Boettger@gmail.com>
2020-12-16 06:17:42 -0500

    add update controller

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using System;
 using System.Collections.Generic;
 using System.Threading;
@@ -34,6 +35,14 @@ namespace API.Controllers
     [HttpPost]
     public async Task<ActionResult<Unit>> Create(Create.Command command)
     {
+      return await _mediator.Send(command);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
+    {
+
+      command.Id = id;
       return await _mediator.Send(command);
     }
   }
