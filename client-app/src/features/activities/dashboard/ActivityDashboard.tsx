c8f11de6353ebf1c81d52438eb2603919b87623d
TMB <Troy.Boettger@gmail.com>
2020-12-20 12:56:17 -0500

    now show listings in ui

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Grid, GridColumn, List } from 'semantic-ui-react'
 import { IActivity } from '../../../app/models/activity'
+import { ActivityList } from './ActivityList'
 
 interface IProps{
 activities: IActivity[]
@@ -10,12 +11,12 @@ const ActivityDashboard: React.FC<IProps> = ({activities}) => {
   return (
       <Grid>
       <Grid.Column width={10}>
-
-          <List>
+      <ActivityList activities={activities} />
+          {/* <List>
           <List.Item>  {activities.map((activity) => (
              <li key={activity.id}>{activity.title}</li>
              ))}</List.Item>
-          </List>
+          </List> */}
         </Grid.Column>
 
       </Grid>
